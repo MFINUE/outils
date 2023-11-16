dae8e8090c9f5caf47b15a13b0f47799f60e16ca
Bora <canboraciner@gmail.com>
2023-11-16 08:18:48 +0300

    Fix redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 export default function Home() {
   const router = useRouter()
-  router.push("/committee")
+  useEffect(() => {
+    if (typeof window !== "undefined") return router.push("/committee")
+  }, [])
   return (
     <>
     </>
